dc0e1822c4495a15d99c20b955d002b5154a4ca6
Jarrett28 <katherine.jarrett@salem.edu>
2023-01-12 17:07:25 -0500

    Code edited to connect to April Tags.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KCKOpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KCKOpenCV.java
@@ -39,10 +39,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera2;
+
+import java.util.ArrayList;
 
 /*
  *
@@ -63,6 +66,28 @@ public class KCKOpenCV extends LinearOpMode {
 
   ColorSensor colorSensor;    // Hardware Device Object
   private OpenCvCamera webcam;
+  AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+  static final double FEET_PER_METER = 3.28084;
+
+  // Lens intrinsics
+  // UNITS ARE PIXELS
+  // NOTE: this calibration is for the C920 webcam at 800x448.
+  // You will need to do your own calibration for other configurations!
+  double fx = 578.272;
+  double fy = 578.272;
+  double cx = 402.145;
+  double cy = 221.506;
+
+  // UNITS ARE METERS
+  double tagsize = 0.166;
+
+  int numFramesWithoutDetection = 0;
+
+  final float DECIMATION_HIGH = 3;
+  final float DECIMATION_LOW = 2;
+  final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
+  final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
 
   @Override
   public void runOpMode() {
@@ -74,38 +99,83 @@ public class KCKOpenCV extends LinearOpMode {
 
     int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
     webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+    aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
-    webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-    {
+    webcam.setPipeline(aprilTagDetectionPipeline);
+    webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
       @Override
-      public void onOpened()
-      {
+      public void onOpened() {
         webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
       }
 
       @Override
-      public void onError(int errorCode)
-      {
+      public void onError(int errorCode) {
         /*
          * This will be called if the camera could not be opened
          */
       }
     });
 
-    webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    telemetry.addLine("Waiting for start");
+    telemetry.update();
 
     // wait for the start button to be pressed.
     waitForStart();
 
+    telemetry.setMsTransmissionInterval(50);
+
     // while the op mode is active, loop and read the RGB data.
     // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
     while (opModeIsActive()) {
+      // Calling getDetectionsUpdate() will only return an object if there was a new frame
+      // processed since the last time we called it. Otherwise, it will return null. This
+      // enables us to only run logic when there has been a new frame, as opposed to the
+      // getLatestDetections() method which will always return an object.
+      ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
+
+      // If there's been a new frame...
+      if (detections != null) {
+        telemetry.addData("FPS", webcam.getFps());
+        telemetry.addData("Overhead ms", webcam.getOverheadTimeMs());
+        telemetry.addData("Pipeline ms", webcam.getPipelineTimeMs());
+        telemetry.addData("Pipeline ms", webcam.getPipelineTimeMs());
 
 
+        // If we don't see any tags
+        if (detections.size() == 0) {
+          numFramesWithoutDetection++;
 
-      telemetry.update();
+          // If we haven't seen a tag for a few frames, lower the decimation
+          // so we can hopefully pick one up if we're e.g. far back
+          if (numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION) {
+            aprilTagDetectionPipeline.setDecimation(DECIMATION_LOW);
+          }
+        }
+        // We do see tags!
+        else {
+          numFramesWithoutDetection = 0;
+
+          // If the target is within 1 meter, turn on high decimation to
+          // increase the frame rate
+          if (detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS) {
+            aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
+          }
+
+          for (AprilTagDetection detection : detections) {
+            telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+            telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x * FEET_PER_METER));
+            telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y * FEET_PER_METER));
+            telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z * FEET_PER_METER));
+            telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+            telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+            telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+          }
+        }
+
+        telemetry.update();
+      }
+
+      sleep(20);
     }
-
-
   }
 }
